7446d046149f7fd20934e29575d0fe3fd229dc19
tqkoh <shktmygs@gmail.com>
2020-08-15 02:30:47 +0900

    [+] 複数ケース同時実行

--- ac/command/abca.py
+++ ac/command/abca.py
@@ -2,7 +2,29 @@
 
 import os
 import readline
+import copy
 from lark import Lark
+
+deb = 1
+samplenum = 1
+gdata = [{}]
+
+def _command_in(names, inputs, outputs):
+	global gdata
+	global samplenum
+	for i in range(1,samplenum):
+		gdata.append(copy.deepcopy(gdata[i-1]))
+	for i in range(samplenum):
+		for j in range(len(names)):
+			try:
+				gdata[i][names[j].lower()] = int(inputs[i][j])
+				if deb >= 2: print("int")
+			except:
+				if deb >= 2: print("string")
+			gdata[i][names[j].upper()] = str(inputs[i][j])
+		gdata[i]["__out"] = str(outputs[i])
+	return
+
 
 class Transformer:
 	def __default__(self, tree, env, d, data):
@@ -13,21 +35,31 @@ class Transformer:
 		return f(tree, d, data)
 
 	def statement(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		for e in tree.children:
 			self.transform(e, d+1, data)
 		return
 
 	def command(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		f = getattr(self, tree.children[0].data, self.__default__)
 		return f(tree.children[1:], d, data)
 
 	def command_in(self, param, d, data):
 		print("  "*d+"input")
-		return
+		global samplenum
+		print(samplenum)
+		if samplenum != 1: return 0, ""
+		names, _ = self.transform(param[0], d+1, data)
+		inputs, _ = self.transform(param[1], d+1, data)
+		outputs, _ = self.transform(param[2], d+1, data)
+		samplenum = len(inputs)
+		_command_in(names, inputs, outputs)
+		return 0, ""
 
 	def command_out(self, param, d, data):
+		print("  "*d+"output")
+		print(samplenum)
 		return self.transform(param[0], d+1, data)
 
 	def command_undo(self, param, d, data):
@@ -39,64 +71,64 @@ class Transformer:
 		exit(0)
 
 	def expr(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr1(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr2(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr3(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr4(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr5(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr6(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr7(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr8(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr9(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr10(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr11(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr12(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	def expr13(self, tree, d, data):
-		# print("  "*d,tree.data)
+		# if deb>=3: print("  "*d,tree.data)
 		return self.transform(tree.children[0], d, data)
 
 	
 	def decl(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		if s in data:
 			print("already defined")
@@ -114,7 +146,7 @@ class Transformer:
 			return data[s], source
 
 	def assign(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" = "
 		r, add = self.transform(tree.children[1], d, data)
@@ -123,7 +155,7 @@ class Transformer:
 		return data[s], source
 
 	def tern(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		l, add = self.transform(tree.children[0], d, data)
 		source = add + " ? "
 		m, add = self.transform(tree.children[1], d, data)
@@ -133,7 +165,7 @@ class Transformer:
 		return m if int(l) else r, source
 
 	def pluseq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" += "
 		r, add = self.transform(tree.children[1], d, data)
@@ -142,7 +174,7 @@ class Transformer:
 		return data[s], source
 
 	def minuseq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" -= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -151,7 +183,7 @@ class Transformer:
 		return data[s], source
 
 	def timeseq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" *= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -160,7 +192,7 @@ class Transformer:
 		return data[s], source
 
 	def diveq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" /= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -169,7 +201,7 @@ class Transformer:
 		return data[s], source
 
 	def divveq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" /= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -178,7 +210,7 @@ class Transformer:
 		return data[s], source
 
 	def poweq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" = pow("+s+", "
 		r, add = self.transform(tree.children[1], d, data)
@@ -187,7 +219,7 @@ class Transformer:
 		return data[s], source
 
 	def modeq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" %= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -196,7 +228,7 @@ class Transformer:
 		return data[s], source
 
 	def lseq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" <<= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -205,7 +237,7 @@ class Transformer:
 		return data[s], source
 
 	def rseq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].children[0].value
 		source = s+" >>= "
 		r, add = self.transform(tree.children[1], d, data)
@@ -214,104 +246,116 @@ class Transformer:
 		return data[s], source
 
 	def _or(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
+
 		return
 
 	def _xor(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def _and(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def _bor(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def _bxor(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def _band(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def eq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def neq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def lt(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def gt(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def leq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def geq(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def ls(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def rs(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def plus(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def minus(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def times(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def div(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def mod(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def pow(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def test(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return
 
 	def value(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		#for e in tree.children:
 		return self.transform(tree.children[0], d+1, data)
 
 	def number(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return int(tree.children[0].value), tree.children[0].value
 
 	def string(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		return tree.children[0].value[1:-1], tree.children[0].value
 	
+	def list(self, tree, d, data):
+		if deb>=3: print("  "*d,tree.data)
+		ret = []
+		source = "["
+		for e in tree.children:
+			r, add = self.transform(e, d, data)
+			ret.append(r)
+			source += ","+add
+		source += "]"
+		return ret, source
+
 	def symbol(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		s = tree.children[0].value
 		if s in data:
 			return data[s], s
@@ -320,7 +364,7 @@ class Transformer:
 			return 0, ""
 
 	def priority(self, tree, d, data):
-		print("  "*d,tree.data)
+		if deb>=3: print("  "*d,tree.data)
 		source = "("
 		m, add = self.transform(tree.children[0], d+1, data)
 		source += add+")"
@@ -331,7 +375,11 @@ def abca(args, config):
 	rule = open('command/sub/abca_grammer.lark').read()
 	parser = Lark(rule, start='statement', parser='lalr')
 	source = ""
-	data = {}
+	global samplenum
+	global gdata
+	samplenum = 1
+	gdata = [{}]
+
 	while 1:
 		statement = input(">> ")
 		try:
@@ -340,8 +388,16 @@ def abca(args, config):
 			print("failed to parse")
 			continue
 
-		res, add = Transformer().transform(tree, 0, data)
-		source += add+";\n"
-		print("ret:",res)
-		print("src:",source)
+		res = [0]*samplenum
+
+		for i in range(samplenum):
+			res[i], add = Transformer().transform(tree, 0, gdata[i])			
+
+		if add != "":
+			source += add+";\n"
+
+		if deb >= 2:
+			print("source:",source,end='')
+		if deb:
+			print("return:",res)
 	return
