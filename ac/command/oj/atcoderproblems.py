0c73f1c130e29dbb7043c93f34e8b8039da3693d
tqkoh <shktmygs@gmail.com>
2020-11-10 02:46:33 +0900

    [>] atcoderproblems.py

--- ac/command/oj/atcoderproblems.py
+++ ac/command/oj/atcoderproblems.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 import os
+import time
+import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -20,22 +22,24 @@ class AtCoderProblems:
 		self.session = driver
 		return driver
 
-	def get_problems(self, contest_id):
+	def get_problems(self, contest_id, f5=0):
 		
 		return
 
 	def get_start_time(self, contest_id):
 		res = self.session.get(f'https://kenkoooo.com/atcoder#/contest/show/{contest_id}')
-		elem = self.session.find_element_by_xpath("/html[@class='theme-dark']/body/div[@id='root']/div/div[@class='my-5 container']/div[@class='my-2 row'][1]/div[@class='col-md-12 col-lg-6'][1]/table[@class='mb-0 table']/tbody/tr[1]/td")
-		start_str = elem.text
-		print(start_str)
-		raise Exception("test")
+		time.sleep(2)
+		with open('test.txt',mode='w') as f: f.write(self.session.page_source)
+		elem = self.session.find_element_by_xpath(f"/html/body/div[@id='root']/div/div[@class='my-5 container']/div[@class='my-2 row'][1]/div[@class='col-md-12 col-lg-6'][1]/table[@class='mb-0 table']/tbody/tr[1]/td")
+		start_str = elem.text[:19]
+		# print(start_str)
 		# start_str = '2020-6-27 2:37:00+0900'
-		return datetime.strptime(start_str[:-5], '%Y-%m-%d %H:%M:%S')
+		return datetime.datetime.strptime(start_str, '%Y-%m-%d %H:%M:%S')
 
 	def get_contest_id(self, url):
 		urlpath = url.fragment.split('/')
 		if len(urlpath) >= 4 and urlpath[1] == 'contest':
+			print(urlpath[3])
 			return urlpath[3]
 		else:
 			return None
