3b510e4906217a2391c97938adfa0efd1b5a7c1d
tqkoh <shktmygs@gmail.com>
2020-11-09 18:29:39 +0900

    [>] clear.py

--- ac/command/clear.py
+++ ac/command/clear.py
@@ -8,11 +8,13 @@ def clear(args,config):
 		for line in source:
 			if line.startswith('#pragma endregion'):
 				sta.pop()
-			if 'solve' not in sta:
+			if 'solve' not in sta and 'main' not in sta:
 				f.write(line);
 			if line.startswith('#pragma region'):
 				s = line.split()
 				sta.append(s[2])
 				if s[2] == 'solve':
-					f.write('\nint main(){\n\t\n}\n\n')
+					f.write('\nint solve(){\n\t\n\treturn 0;\n}\n\n')
+				if s[2] == 'main':
+					f.write('\nint main(){\n\tsolve();\n}\n\n')
 	return
