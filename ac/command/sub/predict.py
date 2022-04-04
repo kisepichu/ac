c48d51d3d77982e51a1e8d9c2dc439fc2eb82e56
tqkoh <shktmygs@gmail.com>
2022-04-04 15:01:29 +0900

    impl predict.py 4/5

--- ac/command/sub/predict.py
+++ ac/command/sub/predict.py
@@ -102,10 +102,11 @@ def expand(ss, vars):
     for s in ss:
         ret.append({"class": "vdots"})
         if s == "":
-            ret[-1]["class"] = "end"
+            ret[-1]["class"] = "empty"
             continue
         if numeral(s[0]):
             ret[-1]["class"] = "query_type"
+            ret[-1]["num"] = s[0]
         elif alphabet(s[0]) or lparen(s[0]):
             ret[-1]["class"] = "var"
             varnum += 1
@@ -136,7 +137,7 @@ def expand(ss, vars):
             if ret[i]["class"] == "var_charlist":
                 name, sub = get_name_sub(ss[i])
                 sub = sub[:-1]
-                vars[name] = {"dim": -1}
+                vars[name] = {"dim": -1, "type": -10}
                 ret[i]["name"] = name
                 ret[i]["sub"], vars[name]["dim"] = make_sub(sub, vars[name]["dim"])
                 vars[name]["dim"] += 1
@@ -147,9 +148,18 @@ def expand(ss, vars):
                     ret[i]["name"] = name
                     ret[i]["sub"], vars[name]["dim"] = make_sub(sub, vars[name]["dim"])
                 else:
-                    if numeral(sub[0]) or name in vars.keys() or query_region:
+                    if numeral(sub[0]) or name in vars.keys():
                         if name not in vars.keys():
                             vars[name] = {"dim": -1}
+                        ret[i]["name"] = name
+                        ret[i]["sub"], vars[name]["dim"] = make_sub(
+                            sub, vars[name]["dim"]
+                        )
+                    elif query_region:
+                        if sub[0] != "i":
+                            name = name + "_" + sub
+                        if name not in vars.keys():
+                            vars[name] = {"dim": 1, "vertical_reducted": 1}
                         ret[i]["name"] = name
                         ret[i]["sub"], vars[name]["dim"] = make_sub(
                             sub, vars[name]["dim"]
@@ -202,7 +212,7 @@ def read_sub(s, i, sd):
         return s[i : i + sd + 1]
 
 
-def vertical_reduction(ex, times):
+def vertical_reduction(ex, times, vars):
     # for es in ex:
     #     print(es)
     # print()
@@ -236,6 +246,8 @@ def vertical_reduction(ex, times):
                                 )
         ex = ex[:1] + [eins] + ex[1:]
     nex += ex[1:2]
+    for j in range(len(nex[1])):
+        vars[nex[1][j]["name"]]["vertical_reducted"] = 1
     for j in range(len(ex[0])):
         e = ex[0][j]
         for key in ["sub", "size", "horizontal_offset"]:
@@ -357,11 +369,32 @@ def guess(vars, constraint):
     return
 
 
+def convert_sub(s):
+    if "_" in s:
+        l = s.split("_")
+
+        l[0] = l[0].replace("{", "").replace("}", "")
+        if l[1][0] != "{":
+            l[1] = "{" + l[1] + "}"
+        l[1] = l[1].replace("{", "[").replace("}", "]")
+        s = "".join(l)
+
+    f = set()
+    for i in range(len(s) - 1):
+        if numeral(s[i]) and alphabet(s[i + 1]):
+            f.add(i)
+    d = 0
+    for e in f:
+        s = s[: e + d + 1] + "*" + s[e + d + 1 :]
+        d += 1
+
+    return s
+
+
 def predict(problem, exs, constraints):
-    success = ""
-    v_times = 0
 
     # format, reduction
+    v_times = 0
     vars = {}
     for ex_i in range(len(exs)):
         ex = exs[ex_i]
@@ -389,15 +422,24 @@ def predict(problem, exs, constraints):
         l = 0
         while l < len(ex):
             r = l + 1
-            while r < len(ex) and (
-                ex[r][0]["class"] == "vdots"
-                or "name" in ex[l][0]
-                and "name" in ex[r][0]
-                and ex[l][0]["name"] == ex[r][0]["name"]
-            ):
+            while r < len(ex):
+                if (
+                    "name" in ex[l][0]
+                    and "name" in ex[r][0]
+                    and ex[l][0]["name"] == ex[r][0]["name"]
+                ):
+                    1
+                else:
+                    ok = 0
+                    for j in range(len(ex[r])):
+                        ok |= ex[r][j]["class"] == "vdots"
+                    if ok:
+                        ex[r][0]["class"] = "vdots"
+                    else:
+                        break
                 r += 1
             if r - l > 1:
-                nex += vertical_reduction(ex[l:r], v_times)
+                nex += vertical_reduction(ex[l:r], v_times, vars)
             else:
                 nex += ex[l:r]
             # print(l, r)
@@ -414,17 +456,30 @@ def predict(problem, exs, constraints):
         input_type = "testcases"
 
     if input_type == "queries":
+        query_vars = set(["q_type"])
+        vars["q_type"] = {"dim": 1, "type": 10}
         for i in range(len(exs[0]) - 2, -1, -1):
             if exs[0][i][0]["class"] == "rep" and exs[0][i + 1][0]["class"] == "var":
                 exs[0][i + 1][0]["class"] = "query"
+                if exs[0][i + 1][0]["name"] in vars.keys():
+                    vars.pop(exs[0][i + 1][0]["name"])
                 break
-
-    for ex in exs:
-        for es in ex:
-            print(es)
-        print()
+        for ex_i in range(1, len(exs)):
+            ex = exs[ex_i]
+            for es_i in range(len(ex)):
+                es = ex[es_i]
+                for e in es:
+                    if (
+                        e["class"] == "var"
+                        or e["class"] == "var_vector"
+                        or e["class"] == "var_charlist"
+                    ):
+                        if vars[e["name"]]["dim"] == 0:
+                            vars[e["name"]]["dim"] = 1
+                        e["sub"] = ["i"]
 
     # get testcases
+
     # test_dir = f"data/testcase/atcoder/{problem[1]}/"
     # tests = []
     # cnt = 0
@@ -438,12 +493,165 @@ def predict(problem, exs, constraints):
     # type
 
     for var, d in vars.items():
-        d["type"] = 0
+        if "type" not in d:
+            d["type"] = 0
+        if var == "S":
+            d["type"] -= 1
+        if var == "T" and "S" in vars.keys():
+            d["type"] -= 0.4
 
     for constraint in constraints:
         guess(vars, constraint)
+
+    # lower
+    # tolower = set()
+    # for var, d in vars.items():
+    #     if var[0].isupper() and var.lower() not in vars.keys():
+    #         tolower.add(var)
+    # for e in tolower:
+    #     vars[e.lower()] = vars[e]
+    #     vars.pop(e)
     print(vars)
 
     # cppify
 
-    return success, "a", "a", "a"
+    success = "1"
+    aargs = ""  # n, m, a
+    fargs = ""  # int n, int m, vector<vector<lint>> a
+    input_part = [""] * len(exs)  # vector<vector<lint>> a(h)
+
+    indent = 1
+
+    def vector_lint(n):
+        if n <= 0:
+            return "lint"
+        return "vector<" + vector_lint(n - 1) + ">"
+
+    def vector_cs(n):
+        if n <= 1:
+            return "cs"
+        return "vector<" + vector_cs(n - 1) + ">"
+
+    for var, d in vars.items():
+        if d["type"] > 0:
+            input_part[0] += "\t" * indent
+            input_part[0] += vector_lint(d["dim"]) + " " + var + ";\n"
+        else:
+            input_part[0] += "\t" * indent
+            if "vertical_reducted" not in d:
+                d["vertical_reducted"] = 0
+            input_part[0] += (
+                vector_cs(min(2, d["dim"] + d["vertical_reducted"])) + " " + var + ";\n"
+            )
+
+    loopf = 0
+
+    for ex_i in range(len(exs) - 1, -1, -1):
+        indent = not ex_i
+        ex = exs[ex_i]
+        for es_i in range(len(ex)):
+            es = ex[es_i]
+            print(es)
+            if es[0]["class"] == "rep":
+                loopf += 1
+                size = convert_sub(es[0]["end"])
+                for i in range(len(ex[es_i + 1])):
+                    if ex[es_i + 1][i]["class"] != "query":
+                        input_part[ex_i] += "\t" * indent
+                        input_part[ex_i] += ex[es_i + 1][i]["name"]
+                        for sub in ex[es_i + 1][i]["sub"]:
+                            if sub != es[0]["loopvar"]:
+                                input_part[ex_i] += "[" + sub + "]"
+                        input_part[ex_i] += ".resize(" + size + ");\n"
+                if ex[es_i + 1][0]["class"] == "query":
+                    for var in query_vars:
+                        input_part[ex_i] += "\t" * indent
+                        input_part[ex_i] += var + ".resize(" + size + ");\n"
+                        if len(ex[es_i + 1][0]["sub"]) == 0:
+                            ex[es_i + 1][0]["sub"] = [es[0]["loopvar"]]
+
+                input_part[ex_i] += "\t" * indent
+                input_part[ex_i] += (
+                    "rep("
+                    + es[0]["loopvar"]
+                    + ", "
+                    + es[0]["begin"]
+                    + ("" if es[0]["begin"] == "0" else "-1")
+                    + ", "
+                    + es[0]["end"]
+                    + ("+1" if es[0]["begin"] == "0" else "")
+                    + "){\n"
+                )
+                indent += 1
+            elif es[0]["class"] == "query":
+                input_part[ex_i] += "\t" * indent
+                input_part[ex_i] += "in(q_type"
+                for sub in es[0]["sub"]:
+                    input_part[ex_i] += "[" + sub + "]"
+                input_part[ex_i] += ");\n"
+                for i in range(1, len(exs)):
+                    input_part[ex_i] += "\t" * indent
+                    input_part[ex_i] += (
+                        "if(q_type["
+                        + ex[es_i - 1][0]["loopvar"]
+                        + "]=="
+                        + exs[i][0][0]["num"]
+                        + "){\n"
+                    )
+                    indent += 1
+                    for line in input_part[i].split("\n")[:-1]:
+                        input_part[ex_i] += "\t" * indent
+                        input_part[ex_i] += line + "\n"
+                    indent -= 1
+                    input_part[ex_i] += "\t" * indent
+                    input_part[ex_i] += "}\n"
+            else:
+                for i in range(len(es)):
+                    if ex_i and input_type == "queries" and "name" in es[i]:
+                        query_vars.add(es[i]["name"])
+                    if es[i]["class"] == "var_vector":
+                        input_part[ex_i] += "\t" * indent
+                        input_part[ex_i] += es[i]["name"]
+                        for sub in es[i]["sub"]:
+                            input_part[ex_i] += "[" + sub + "]"
+                        input_part[ex_i] += (
+                            ".resize(" + convert_sub(es[i]["size"][0]) + ");\n"
+                        )
+                    if (
+                        es[i]["class"] == "var_vector"
+                        and es[i]["horizontal_offset"][0] != "1"
+                    ):
+                        input_part[ex_i] += "\t" * indent
+                        input_part[ex_i] += (
+                            "rep(_, "
+                            + es[0]["horizontal_offset"][0]
+                            + ", "
+                            + convert_sub(es[i]["size"][0])
+                            + ")in("
+                            + es[i]["name"]
+                        )
+                        for sub in es[i]["sub"]:
+                            input_part[ex_i] += "[" + sub + "]"
+                        input_part[ex_i] += "[_]);\n"
+                    elif (
+                        es[i]["class"] == "var"
+                        or es[i]["class"] == "var_charlist"
+                        or es[i]["class"] == "var_vector"
+                    ):
+                        input_part[ex_i] += "\t" * indent
+                        input_part[ex_i] += "in(" + es[i]["name"]
+                        for sub in es[i]["sub"]:
+                            input_part[ex_i] += "[" + sub + "]"
+                        input_part[ex_i] += ");\n"
+                if loopf:
+                    loopf -= 1
+                    indent -= 1
+                    input_part[ex_i] += "\t" * indent
+                    input_part[ex_i] += "}\n"
+
+        # print()
+
+    for source in input_part:
+        print(source)
+        print()
+    return success, aargs, fargs, input_part[0]
